02a23ff75fdd1644f90722f8d0d5a21cfde7e8d2
fazarrr <fazarbahari25@gmail.com>
2022-06-12 15:31:19 +0700

    commit tugas advance callback

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,72 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+// handle untuk mengambil kata kedua dari file yang di baca
+const getKataKedua = (kata) => {
+  const splitKata = kata.split(' ')
+
+  if (splitKata?.length >= 1) {
+    return splitKata[1]
+  }
+}
+
+// baca data dengan callback fungsi getKataKedua
+const prosesData = (dataParsing) => {
+  const data = JSON.parse(dataParsing)
+  let getKataString = ''
+
+  // case object
+  if (data?.message !== undefined) {
+    getKataString = data?.message
+  }
+
+  // case array
+  if (data?.length) {
+    data?.forEach(item => {
+      
+      if (item?.message !== undefined) {
+        getKataString = item?.message
+      }
+
+      if (item?.data?.message !== undefined) {
+        getKataString = item?.data?.message
+      }
+    })
+  }
+
+  return getKataKedua(getKataString)
+}
+
+
+// baca data dengan callback fungsi prosesData
+const bacaData = (fnCallback) => {
+  const listFile = [file1, file2, file3]
+  const result = []
+
+  listFile.forEach(item => {
+    const processItem = new Promise((resolve, reject) => {
+      fs.readFile(item, (error, data) => {
+        // handle bagian eror
+        if (error) {
+          reject(error)
+          return
+        }
+        // handle bagian sukses
+        const getProcessedItem = prosesData(data)
+        resolve(getProcessedItem)
+      })
+    })
+    result.push(processItem)
+  })
+
+  // memanggil callback result
+  Promise.all(result).then(values => {
+    fnCallback(null, values)
+  }).catch(error => {
+    fnCallback(error, null)
+  });
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
